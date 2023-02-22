c33a03f832884b8ce9f19b199b81a0c42945b7c9
khanhdt2001 <khanhrhino1607@gmail.com>
2023-02-23 01:36:10 +0700

    feature make request

---
+++ src/data/nft.data.js
@@ -0,0 +1,19 @@
+export const nftData = [
+    {
+        localAddress: "0x0aaaaaaaaaa",
+        webAddress : "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
+        isAble : true
+    },
+    {
+        localAddress: "0x1aaaaaaaaaa",
+        webAddress : "0x60E4d786628Fea6478F785A6d7e704777c86a7c6",
+        isAble : true
+    },
+    {
+        localAddress: "0x2aaaaaaaaaa",
+        webAddress : "0x6EFc003D3F3658383F06185503340C2Cf27A57b6",
+        isAble : true
+    },
+
+]
+
