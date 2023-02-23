2e6c956a4b10823905c2322d40411399fd28c972
khanhdt <khanhdt@smartosc.com>
2023-02-23 13:56:01 +0700

    ipfs

--- src/function/Function.js
+++ src/function/Function.js
@@ -1,3 +1,5 @@
+import {Alchemy, Network} from "alchemy-sdk";
+
 const { ethereum } = window;
 // const Web3 = require("web3");
 // const web3 = new Web3(Web3.givenProvider);
@@ -21,4 +23,19 @@ export const myShortString = (message, number) => {
         response = prefix + "..." +endfix
     }
     return response
+}
+
+const settings = {
+    apiKey: "-C7_ur_4s6R0oUnBNGi4qB3XWfv-HsFR",
+    network: Network.ETH_MAINNET,
+};
+
+export const alchemy = new Alchemy(settings);
+
+export const convertIpfs = (string) => {
+    if (string) {
+        const res = string.replace("ipfs://", "https://ipfs.io/ipfs/")
+        return res
+    }
+    return "error"
 }
